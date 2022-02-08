cbbd13705ebd5621f26311196b5fcd47999d819c
전은형 <ooooo5h91@gmail.com>
2022-02-08 18:15:03 +0900

    url 경로를 추가
    여기까지하고 포스트맨응로 날리면, json과 같은 형태로 응답이 성공적으로 내려옴

--- DjangWithMysns/urls.py
+++ DjangWithMysns/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from my_sns.api.user import User
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user', User.as_view(), name='user'),
 ]
