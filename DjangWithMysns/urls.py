90ada9851c4dfda80cf010d7c5f07ccfc99a1d6d
전은형 <ooooo5h91@gmail.com>
2022-02-08 21:42:52 +0900

    스웨거 문서의 정보사항을 작성함

--- DjangWithMysns/urls.py
+++ DjangWithMysns/urls.py
@@ -17,6 +17,22 @@ from django.contrib import admin
 from django.urls import path
 from my_sns.api.user import User
 
+# swagger 관련 모듈 import
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+
+# swagger에 적는 프로젝트의 전반적인 정보를 기재
+schema_view = get_schema_view(
+    openapi.Info(
+        title='장고 테스트 - MySNS',
+        default_version='v1',
+        description='장고를 이용해서 생성했음'
+    ),
+    public=True,
+    permission_classes=(AllowAny)   
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user', User.as_view(), name='user'),
